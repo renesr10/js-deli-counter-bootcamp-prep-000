84f0e2f4e903c65ae9e9a752188d2e8e3f763cd7
Rene Santana <renesantana10@gmail.com>
2020-01-20 18:15:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0)
-  return `Currently serving ${katzDeliLine.shift()}.`; else return 'There is nobody waiting to be served!'
+  return `Currently serving ${katzDeliLine.shift()}.`; else return 'There is nobody waiting to be served!';
 } 
 
 function currentLine(katz) {
-  var string = 'The line is currently: '
-  if (katz.length === 0){ 
-  var newKatz = []; 
- for (var i = 0; i < katz.length; i++);{
-  newKatz.push(i++ + '. ' + katz[i]);}
- return string + newKatz.join(', ');
-} else {return 'The line is currently empty.';}
+  if (katz.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    var newKatz = [];
+    for(var i = 0; i < katz.length; i++)
+    newKatz.push(` ${i+1}. ${katz[i]}`);
+} return 'The line is currently:' + newKatz;
 }
